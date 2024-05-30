babdcc31d9d33f8146b6239195f5dbdebd9df638
Jeremy Merlin <jeremymerlin@hotmail.fr>
2024-05-30 14:50:49 +0100

    fix

--- src/utils/db.jsx
+++ src/utils/db.jsx
@@ -2,12 +2,18 @@ import axios from "axios";
 
 export async function getArtworks() {
   try {
-    const data = await axios.get("https://openaccess-api.clevelandart.org/api/artworks/", {
+    const config = {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
       params: {
         fields: "id,current_location,title,creation_date,collection,type,description,url,images",
       },
-    });
-    return data;
+    };
+
+    const response = await axios.get("https://openaccess-api.clevelandart.org/api/artworks/", config);
+
+    return response.data;
   } catch (error) {
     throw error;
   }
